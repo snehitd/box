6a5877587e3017ddec5c7d53d315d9d9707034da
snehitd <48319450+snehitd@users.noreply.github.com>
2021-01-07 18:55:11 +0530

    Add files via upload

---
+++ Box.js
@@ -0,0 +1,16 @@
+class Box{
+    constructor(x,y,w,h){
+        var options={
+            isStatic: true
+        }
+        this.body=Bodies.rectangle(x,y,w,h,options);
+        World.add(world,this.body);
+    }
+
+    display(){
+        var pos=this.body.position;
+        rectMode(CENTER);
+        fill(255);
+        rect(pos.x,pos.y,this.width,this.height);
+    }
+}
